32d9d18c3c9e952668c60c43b4f614e83b02e900
Reuben Mathew <intelphysic@gmail.com>
2020-01-20 14:40:34 -0500

    Main fxml can be run through MainApp

--- VennFX/src/application/MainApp.java
+++ VennFX/src/application/MainApp.java
@@ -1,34 +1,51 @@
 package application;
-	
+
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-
-
-public class Main extends Application {
+public class MainApp extends Application {
 	
-	private BorderPane rootLayout;
+	Stage primaryStage;
+	private AnchorPane rootLayout;
+
 	@Override
 	public void start(Stage primaryStage) {
+		
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Main Application");
+		
+		initRootLayout();
+		
+	}
+	
+	public void initRootLayout() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(getClass().getResource("Main.fxml"));
+			rootLayout = (AnchorPane) loader.load();
 			
-			rootLayout = (BorderPane) loader.load();
-			//Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
 			Scene scene = new Scene(rootLayout);
-			primaryStage.setTitle("MY SCENE");
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+			
+			
+		}
+		catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	
+	public Stage getPrimaryStage() {
+		
+		return primaryStage;
+	}
 	public static void main(String[] args) {
 		launch(args);
 	}
