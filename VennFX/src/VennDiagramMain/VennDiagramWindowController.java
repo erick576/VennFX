33f39a87965d88c0985d8e34b8415885aa6a4eee
Eric Kwok <46385457+Kwok-Eric-216397150@users.noreply.github.com>
2020-01-27 17:32:16 -0500

    Added VennDiagram Controller Class

---
+++ VennFX/src/VennDiagramMain/VennDiagramWindowController.java
@@ -0,0 +1,23 @@
+package VennDiagramMain;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+public class VennDiagramWindowController implements Initializable{
+
+	public TextField entryText;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+	}
+	
+	
+	public void entryButton(ActionEvent event) {
+		
+	}
+	
+}
