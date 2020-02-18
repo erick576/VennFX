4805efb28b1e91b208aafacc8ec290c214d2034e
Eric Kwok <46385457+Kwok-Eric-216397150@users.noreply.github.com>
2020-02-17 22:38:47 -0500

    Enhancments

--- VennFX/src/VennDiagramMain/VennDiagramWindowController.java
+++ VennFX/src/VennDiagramMain/VennDiagramWindowController.java
@@ -1,25 +1,32 @@
 package VennDiagramMain;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.ResourceBundle;
 import javax.swing.JOptionPane;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class VennDiagramWindowController implements Initializable {
 
+	@FXML 
+	public ColorPicker color1, color2;
+	
 	@FXML
-	public TextField textField, title1, title2, side;
+	public TextField textField, title1, title2;
 
 	@FXML
 	public StackPane stackPane;
@@ -31,100 +38,39 @@ public class VennDiagramWindowController implements Initializable {
 	public Circle circle2;
 
 	@FXML
-	public Button entryButton, title1Button, title2Button;
+	public Button entryButton, title1Button, title2Button, change1Button, change2Button;
 
 	@FXML
 	public VBox Abox, Bbox, ABbox;
+
+	@FXML
+	public ChoiceBox<String> sides;
 
 	double orgSceneX, orgSceneY;
 	double orgTranslateX, orgTranslateY;
 	public static HashSet<String> entriesAB = new HashSet<>();
 	public static HashSet<String> entriesA = new HashSet<>();
 	public static HashSet<String> entriesB = new HashSet<>();
+	ObservableList<String> sidesList = FXCollections.observableArrayList("Left Side", "Right Side", "Middle", "");
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		loadData();
 	}
 
 	public void entryButton(ActionEvent event) {
 		try {
 
-			// Testing For Invalid Statements
-			/*
-			 * Situations that will throw an invalid statement 
-			 * 1.Empty Text 
-			 * 2.Text Length Over 100 
-			 * 3.Word Length Over 20 
-			 * 4.If a word is not All CAPS, NO CAPS, or ONLY
-			 * THE FIRST LETTER CAPS
-			 */
-			String testSpaces = textField.getText();
-			if (textField.getText().length() > 100
-					|| (testSpaces.replaceAll("", " ").length() == textField.getText().length()
-							&& textField.getText().length() > 20)
-					|| textField.getText().equals("")) {
+			if (textField.getText().contentEquals("")) {
 				throw new Exception();
 			}
 
-			int wordLength = 0;
-			int capCount = 0;
-			boolean isFirst = false;
-			ArrayList<Integer> lengths = new ArrayList<Integer>();
-			for (int i = 0; i < textField.getText().length(); i++) {
-				if (textField.getText().charAt(i) != ' ') {
-					wordLength++;
-				}
-
-				if (Character.isUpperCase(textField.getText().charAt(i)) && wordLength == 1) {
-					capCount++;
-					isFirst = true;
-					continue;
-				}
-
-				if (Character.isUpperCase(textField.getText().charAt(i))) {
-					capCount++;
-				}
-
-				if (textField.getText().charAt(i) == ' ') {
-					lengths.add(wordLength);
-					if (capCount != wordLength
-							&& ((capCount == 1 && isFirst == false) || (capCount != 1 && capCount != 0))) {
-						throw new Exception();
-					}
-					capCount = 0;
-					wordLength = 0;
-				}
-				if (i == textField.getText().length() - 1) {
-					lengths.add(wordLength);
-					if (capCount != wordLength
-							&& ((capCount == 1 && isFirst == false) || (capCount != 1 && capCount != 0))) {
-						throw new Exception();
-					}
-					capCount = 0;
-					wordLength = 0;
-				}
-			}
-			for (int i : lengths) {
-				if (i > 20) {
-					throw new Exception();
-				}
-			}
-			
-			// If the side textbox is invalid
-			if (!side.getText().contentEquals("") && !side.getText().contentEquals("a")
-					&& !side.getText().contentEquals("A") && !side.getText().contentEquals("B")
-					&& !side.getText().contentEquals("b") && !side.getText().contentEquals("ab")
-					&& !side.getText().contentEquals("AB")) {
-				throw new Exception();
-			}
-			
-			
 			// Add Entry into the set
-			//When hotZones are inplemented and dragged into a cerain zone add the text to the set like below for example
-			//entriesA.add(textField.getText());
-			
+			// When hotZones are inplemented and dragged into a cerain zone add the text to
+			// the set like below for example
+			// entriesA.add(textField.getText());
+
 			// Input Valid Text File Into Drag and Drop TextBox
-			
 			TextField entry = new TextField();
 			entry.autosize();
 			entry.setText(textField.getText());
@@ -162,184 +108,110 @@ public class VennDiagramWindowController implements Initializable {
 			});
 
 			// Dragging into HotZone Functionallity
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
 
-			
-			// If Side is already specified then there is no need to drag, the text box will automatically fall in the hotZone
-			if (!side.getText().equals("")) {
-				if (side.getText().contentEquals("A") || side.getText().contentEquals("a")) {
-					Abox.getChildren().add(entry);
-				}
-				else if (side.getText().contentEquals("B") || side.getText().contentEquals("b")) {
-					Bbox.getChildren().add(entry);
-				}
-				else if (side.getText().contentEquals("AB") || side.getText().contentEquals("ab")) {
-					entry.setMaxWidth(150);
-					ABbox.getChildren().add(entry);
-				}
-				side.setText("");
+			// If Side is already specified then there is no need to drag, the text box will
+			// automatically fall in the hotZone
+			if (sides.getValue().contentEquals("Left Side")) {
+				Abox.getChildren().add(entry);
+			} else if (sides.getValue().contentEquals("Right Side")) {
+				Bbox.getChildren().add(entry);
+			} else if (sides.getValue().contentEquals("Middle")) {
+				entry.setMaxWidth(150);
+				ABbox.getChildren().add(entry);
 			}
+			sides.setValue("");
 
 		} catch (Exception e1) {
 			JOptionPane.showMessageDialog(null, "Please enter a valid entry");
 			textField.setText("");
-			side.setText("");
 		}
 	}
 
+	// Set Title1
 	public void title1Button(ActionEvent event) {
 		try {
-			
-			// Testing For Invalid Statements
-			/*
-			 * Situations that will throw an invalid statement 
-			 * 1.Empty Text 
-			 * 2.Text Length Over 100 
-			 * 3.Word Length Over 20 
-			 * 4.If a word is not All CAPS, NO CAPS, or ONLY
-			 * THE FIRST LETTER CAPS
-			 */
-			
-			String testSpaces = title1.getText();
-			if (title1.getText().length() > 100 || (testSpaces.replaceAll("", " ").length() == title1.getText().length()
-					&& title1.getText().length() > 20) || title1.getText().equals("")) {
+
+			if (title1.getText().contentEquals("")) {
 				throw new Exception();
 			}
 
-			int wordLength = 0;
-			int capCount = 0;
-			boolean isFirst = false;
-			ArrayList<Integer> lengths = new ArrayList<Integer>();
-			for (int i = 0; i < title1.getText().length(); i++) {
-				if (title1.getText().charAt(i) != ' ') {
-					wordLength++;
-				}
-
-				if (Character.isUpperCase(title1.getText().charAt(i)) && wordLength == 1) {
-					capCount++;
-					isFirst = true;
-					continue;
-				}
-
-				if (Character.isUpperCase(title1.getText().charAt(i))) {
-					capCount++;
-				}
-
-				if (title1.getText().charAt(i) == ' ') {
-					lengths.add(wordLength);
-					if (capCount != wordLength
-							&& ((capCount == 1 && isFirst == false) || (capCount != 1 && capCount != 0))) {
-						throw new Exception();
-					}
-					capCount = 0;
-					wordLength = 0;
-				}
-				if (i == title1.getText().length() - 1) {
-					lengths.add(wordLength);
-					if (capCount != wordLength
-							&& ((capCount == 1 && isFirst == false) || (capCount != 1 && capCount != 0))) {
-						throw new Exception();
-					}
-					capCount = 0;
-					wordLength = 0;
-				}
-			}
-			for (int i : lengths) {
-				if (i > 20) {
-					throw new Exception();
-				}
-			}
-			
-			
-			//Set Text
+			// Set Text
+			JOptionPane.showMessageDialog(null, "Title Is Now Locked");
 			title1.setEditable(false);
-			
+
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Please enter a valid entry");
 			title1.setText("");
 		}
 	}
 
+	// Set Title2
 	public void title2Button(ActionEvent event) {
 		try {
-			
-			// Testing For Invalid Statements
-			/*
-			 * Situations that will throw an invalid statement 
-			 * 1.Empty Text 
-			 * 2.Text Length Over 100 
-			 * 3.Word Length Over 20 
-			 * 4.If a word is not All CAPS, NO CAPS, or ONLY
-			 * THE FIRST LETTER CAPS
-			 */
-			
-			String testSpaces = title2.getText();
-			if (title2.getText().length() > 100 || (testSpaces.replaceAll("", " ").length() == title2.getText().length()
-					&& title2.getText().length() > 20) || title2.getText().equals("")) {
+
+			if (title2.getText().contentEquals("")) {
 				throw new Exception();
 			}
 
-			int wordLength = 0;
-			int capCount = 0;
-			boolean isFirst = false;
-			ArrayList<Integer> lengths = new ArrayList<Integer>();
-			for (int i = 0; i < title2.getText().length(); i++) {
-				if (title2.getText().charAt(i) != ' ') {
-					wordLength++;
-				}
-
-				if (Character.isUpperCase(title2.getText().charAt(i)) && wordLength == 1) {
-					capCount++;
-					isFirst = true;
-					continue;
-				}
-
-				if (Character.isUpperCase(title2.getText().charAt(i))) {
-					capCount++;
-				}
-
-				if (title2.getText().charAt(i) == ' ') {
-					lengths.add(wordLength);
-					if (capCount != wordLength
-							&& ((capCount == 1 && isFirst == false) || (capCount != 1 && capCount != 0))) {
-						throw new Exception();
-					}
-					capCount = 0;
-					wordLength = 0;
-				}
-				if (i == title2.getText().length() - 1) {
-					lengths.add(wordLength);
-					if (capCount != wordLength
-							&& ((capCount == 1 && isFirst == false) || (capCount != 1 && capCount != 0))) {
-						throw new Exception();
-					}
-					capCount = 0;
-					wordLength = 0;
-				}
-			}
-			for (int i : lengths) {
-				if (i > 20) {
-					throw new Exception();
-				}
-			}
-			
-			//Set Text
+			// Set Text
+			JOptionPane.showMessageDialog(null, "Title Is Now Locked");
 			title2.setEditable(false);
-			
+
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Please enter a valid entry");
 			title2.setText("");
+		}
+	}
+
+	// Change Title1
+	public void change1Button(ActionEvent event) {
+		try {
+			if (title1.getText().contentEquals("")) {
+				throw new Exception();
+			}
+			title1.setEditable(true);
+			JOptionPane.showMessageDialog(null, "Title Is Now Unocked");
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Please enter a valid entry");
+			title1.setText("");
+		}
+	}
+
+	// Change Title2
+	public void change2Button(ActionEvent event) {
+		try {
+			if (title2.getText().contentEquals("")) {
+				throw new Exception();
+			}
+
+			JOptionPane.showMessageDialog(null, "Title Is Now Unocked");
+			title2.setEditable(true);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Please enter a valid entry");
+			title2.setText("");
+		}
+	}
+
+	private void loadData() {
+		sides.setValue("");
+		sides.setItems(sidesList);
+	}
+
+	public void color1(ActionEvent event) {
+		try {
+			Color selectedColor = color1.getValue();
+			circle1.setFill(selectedColor);
+		} catch (Exception e) {
+
+		}
+	}
+
+	public void color2(ActionEvent event) {
+		try {
+			Color selectedColor = color2.getValue();
+			circle2.setFill(selectedColor);
+		} catch (Exception e) {
+
 		}
 	}
 
